4882cfe043bed0f33708cd73dfc5248b96f9e021
Azad singh <azadsinghrav@gmail.com>
2023-09-18 11:14:29 +0530

    commeted change
    commeted change

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+/*
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 60) {
+        $("header").addClass("header-chng");
+    }
+    else{
+        $("header").removeClass("header-chng");
+    }
+})*/
+
+
+
